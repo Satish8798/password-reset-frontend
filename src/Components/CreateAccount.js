9edee59283b92c0f875f3cb9bd11827e3a79a42f
Satish Ganesan <ganesansatishm7@gmail.com>
2022-11-21 18:15:30 +0530

    updated reset page

--- src/Components/CreateAccount.js
+++ src/Components/CreateAccount.js
@@ -1,19 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+
 const CreateAccount = () => {
-    return ( 
-        <div>
-            <form className="mt-4 p-2 w-25 ms-5 w-md-100">
-                    <div className="form-floating mb-3 ">
-                        <input type="email" name="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label for="floatingInput">Email</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="password" name="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                        <label for="floatingPassword">Password</label>
-                    </div>
-                    <button type="submit" className="btn btn-success mt-3">Create</button>
-            </form>
+  const [accountData, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [createStatus, setCreateStatus] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      let response = await axios.post("http://localhost:8000/create-account", {
+        ...accountData
+      });
+      setCreateStatus(response.data.msg);
+      setData({
+        email: "",
+    password: "",
+      })
+      setInterval(()=>{
+        setCreateStatus(false);
+      },3000)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <form
+        className="mt-4 p-2 w-25 ms-5 w-md-100"
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <div className="form-floating mb-3 ">
+          <input
+            type="email"
+            value={accountData.email}
+            onChange={(e) => {
+              setData({ ...accountData, email: e.target.value });
+            }}
+            className="form-control"
+            id="floatingInput"
+            required
+          />
+          <label for="floatingInput">Email</label>
         </div>
-     );
-}
- 
+        <div className="form-floating">
+          <input
+            type="password"
+            onChange={(e) => {
+              setData({ ...accountData, password: e.target.value });
+            }}
+            value={accountData.password}
+            className="form-control"
+            id="floatingPassword"
+            required
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button type="submit" className="btn btn-success mt-3">
+          Create
+        </button>
+      </form>
+        {createStatus &&
+            <div class="alert alert-success w-25 ms-5" role="alert">
+            account created!!
+        </div>
+        }
+    </div>
+  );
+};
+
 export default CreateAccount;
--- src/Components/PasswordReset.js
+++ src/Components/PasswordReset.js
@@ -1,25 +1,114 @@
+import axios from "axios";
+import { useState } from "react";
+
 const PasswordReset = () => {
-    return ( 
-        <div>
-            <form className="mt-4 p-2 w-25 ms-5 w-md-100">
-                    <div className="form-floating mb-3 ">
-                        <input type="email" name="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label for="floatingInput">Email</label>
-                    </div>
-                    <div className="password-change">
-                        <div className="form-floating  mb-3">
-                            <input type="password" name="email" className="form-control" id="floatingPassword" placeholder="Password"/>
-                            <label for="floatingPassword">New Password</label>
-                        </div>
-                        <div className="form-floating">
-                            <input type="password" name="confirm" className="form-control" id="floatingPassword" placeholder="Password"/>
-                            <label for="floatingPassword">Confirm Password</label>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn btn-success mt-3">Reset</button>
-            </form>
+  const [accountData, setData] = useState({
+    email: "",
+    newPassword: "",
+    confirmPassword: null,
+  });
+  const [passwordChangeStatus, setPasswordChangeStatus] = useState(false);
+  const [alert , setAlert] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!accountData.confirmPassword) {
+      try {
+        let response = await axios.post(
+          "http://localhost:8000/search-account",
+          {
+            ...accountData,
+          }
+        );
+        console.log(response)
+        if(response.data.msg){
+            setPasswordChangeStatus(true);
+        }else{
+            setAlert(true);
+            setTimeout(()=>{
+                setData({...accountData,email:""})
+                console.log(accountData)
+                setAlert(false);
+                
+            },3000);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <form
+        className="mt-4 p-2 w-25 ms-5 w-md-100"
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <div className="form-floating mb-3 ">
+          <input
+            type="email"
+            onChange={(e) => {
+              setData({ ...accountData, email: e.target.value });
+            }}
+            className="form-control"
+            id="floatingInput"
+            value={accountData.email}
+            required
+          />
+          <label for="floatingInput">Email</label>
         </div>
-     );
-}
- 
+        {passwordChangeStatus && (
+          <div className="password-change">
+            <div className="form-floating  mb-3">
+              <input
+                type="password"
+                onChange={(e) => {
+                  setData({ ...accountData, newPassword: e.target.value });
+                }}
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+                required
+              />
+              <label for="floatingPassword">New Password</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                onChange={(e) => {
+                  setData({ ...accountData, confirmPassword: e.target.value });
+                }}
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+                required
+              />
+              <label for="floatingPassword">Confirm Password</label>
+            </div>
+            <button type="submit" className="btn btn-success mt-3">
+              Reset
+            </button>
+          </div>
+        )}
+        {!passwordChangeStatus && (
+          <button type="submit" className="btn btn-success mt-3">
+            Search
+          </button>
+        )}
+        {
+            alert &&
+            (  
+                <div class="alert alert-success w-75 mt-3 ms-1" role="alert">
+                    account not found!!
+                </div>
+
+            )
+        }
+      </form>
+    </div>
+  );
+};
+
 export default PasswordReset;
